1717e338b2218640e23159437b8bdda75e77e73f
Chon Lok Lei <chonloklei@gmail.com>
2021-03-03 18:31:03 +0800

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -5,4 +5,4 @@ python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate
 python3 -m pip install --upgrade pip
-python3 -m pip -r requirements.txt
+python3 -m pip install -r requirements.txt
