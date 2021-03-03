3650cc423a677db41c7bc3d3846bc0d7ce79dc1d
Chon Lok Lei <chonloklei@gmail.com>
2021-03-03 16:05:22 +0800

    Use requirements for installation

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ python3 -m pip install --user --upgrade pip
 python3 -m pip install --user virtualenv
 python3 -m venv env
 source env/bin/activate
-pip install --upgrade pip
-pip3 install myokit  # Get Myokit, might need external installation for sundials
-pip3 install git+https://github.com/pints-team/pints
+python3 -m pip install --upgrade pip
+python3 -m pip -r requirements.txt
